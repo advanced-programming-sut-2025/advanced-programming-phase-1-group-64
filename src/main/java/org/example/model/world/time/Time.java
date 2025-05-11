ef1713b07d7d0755c73d907f3eb12e895bf8688c
MahdiShokoufi <mahdishokoufi@gmail.com>
2025-05-12 00:25:45 +0330

    Time Functions

--- src/main/java/org/example/model/world/time/Time.java
+++ src/main/java/org/example/model/world/time/Time.java
@@ -6,12 +6,38 @@ public class Time {
     public static final int START_HOUR = 9;
     public static final int HOURS_PER_DAY = 14;
     public static final int DAYS_PER_SEASON = 28;
+    public static final int SEASON_CYCLE_HOURS = DAYS_PER_SEASON * HOURS_PER_DAY;
 
-//    public int getHourOfDay(){}
-//    public int getDayIndex(){}
-//    public int getDayOfWeek(){}
-//    public SeasonType getSeason(){}
 
-    public void advanceHours(int hours){}
-    public void advanceDays(int days){}
+    public Time() {
+        this.elapsedHours = 0;
+    }
+
+    public int getHourOfDay(){
+        return START_HOUR + (elapsedHours % HOURS_PER_DAY);
+    }
+    public int getDayIndex(){
+        return (elapsedHours / HOURS_PER_DAY % DAYS_PER_SEASON) + 1;
+    }
+    public int getDayOfWeek(){
+        return (elapsedHours / HOURS_PER_DAY % 7) + 1;
+    }
+    public SeasonType getSeason(){
+        int totalSeasons = SeasonType.values().length;
+        int seasonIndex = (elapsedHours / SEASON_CYCLE_HOURS) % totalSeasons;
+        return SeasonType.values()[seasonIndex];
+    }
+
+    public void advanceHours(int hours){
+        if (hours < 0) {
+            throw new IllegalArgumentException("Hours cannot be negative");
+        }
+        this.elapsedHours += hours;
+    }
+    public void advanceDays(int days){
+        if (days < 0) {
+            throw new IllegalArgumentException("Days cannot be negative");
+        }
+        this.elapsedHours += days * HOURS_PER_DAY;
+    }
 }
