e572c27015446562753db1f4daeee679ec158d07
AmirMOHNasiri <amirsana8587@gmail.com>
2025-05-14 18:24:28 +0330

    main menu

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -2,11 +2,14 @@ package org.example.controller;
 
 import org.example.config.UserRepository;
 import org.example.model.Result;
+import org.example.model.characters.Player;
 import org.example.model.context.App;
 import org.example.model.menus.Menu;
 
 public class MainController {
     public Result logout() {
+        Player player = App.getCurrentPlayer();
+        player.setStayLoggedIn(false);
         App.setCurrentPlayer(null);
         App.setCurrentMenu(Menu.REGISTER_MENU);
         return new Result(true, "Logged out");
--- src/main/java/org/example/view/MainMenu.java
+++ src/main/java/org/example/view/MainMenu.java
@@ -1,6 +1,10 @@
 package org.example.view;
 
 import org.example.controller.MainController;
+import org.example.model.context.App;
+import org.example.model.menus.LoginCommands;
+import org.example.model.menus.MainCommands;
+import org.example.model.menus.Menu;
 
 import java.util.regex.Matcher;
 
@@ -9,8 +13,37 @@ public class MainMenu implements AppMenu{
 
     @Override public void check(String input) {
         Matcher matcher = null;
+        if((matcher= MainCommands.LOGOUT.getMatcher(input))!=null){
+            System.out.println(controller.logout());
+        }else if((matcher= MainCommands.SHOW_MENU.getMatcher(input))!=null) {
+            showCurrentMenu();
+        }else if((matcher= MainCommands.MENU_ENTER.getMatcher(input))!=null) {
+            menuEnter(matcher.group("menu"));
+        }else if((matcher= MainCommands.EXIT.getMatcher(input))!=null) {
+            menuExit();
+        }else {
+            System.out.println("Invalid command");
+        }
     }
-    @Override public void showCurrentMenu() {}
-    @Override public void menuEnter(String menuName) {}
-    @Override public void menuExit() {}
+    @Override public void showCurrentMenu() {
+        System.out.println("main menu");
+    }
+    @Override public void menuEnter(String menuName) {
+        switch (menuName) {
+            case "profile":
+                App.setCurrentMenu(Menu.REGISTER_MENU);
+                System.out.println("We are now in profile menu");
+                break;
+            case "game":
+                App.setCurrentMenu(Menu.GAME_MENU);
+                System.out.println("We are now in game menu");
+                break;
+            default:
+                System.out.println("can't go here");
+                break;
+        }
+    }
+    @Override public void menuExit() {
+        App.setCurrentMenu(Menu.EXIT_MENU);
+    }
 }
